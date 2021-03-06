95212ae5557bbfdaf2481f094e533682aef29ead
JS <jsedrak@student.ie.edu>
2021-03-06 08:30:42 -0500

    First Commit With Strings

---
+++ stingsllices.py
@@ -0,0 +1,9 @@
+s = "Hello, how are you guys?"
+print(s[2:4]+"Cool J")
+print(s[:6]+"my name is Slim Shady")
+print(s[15:] + " are crazy")
+s = "0123456789"
+print(s[2:7])
+print(s[2::3])
+print(s[8:2:-1])
+print(s[::-3])
---
+++ stringoperations.py
@@ -0,0 +1,17 @@
+hi = "Hello"
+print(hi[2])
+print("Hello"[2])
+if hi[2] == hi[3]:
+    print("This is true because we have double l")
+greeting = "hi" + " " + "how " + "are " + "you " + "?"
+print(greeting)
+#multiplication
+print("bye"*3)
+if "hi" > "HI":
+    print("hi > HI")
+else:
+    print("hi < HI")
+print("1ello" > "hello")
+print("hellooo" > "hello")
+print("ana" in "banana")
+
